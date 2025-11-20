8b971911ca15c703eb4ff0bccd96a1514fc32b4c
Khanh Tran <ankhanhtran02@gmail.com>
2025-11-20 21:55:47 +0700

    Change bash script

--- scripts/run_train_TaSL_T5_codetask.sh
+++ scripts/run_train_TaSL_T5_codetask.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 
-begin_id=2
+begin_id=0
 
 
 for ((ORDER=$begin_id; ORDER<4; ORDER++))
 do
 
     python finetune_continualDST_T5_codetask.py \
-        --num_epochs 1 \
-        --train_size 10 \
-        --val_size 10 \
+        --num_epochs 5 \
+        --train_size -1 \
+        --val_size 100 \
         --task_list CONCODE CodeTrans CodeSearchNet BFP \
         --service_begin_id=${ORDER} \
+        --train_batch_size 8 \
+        --eval_batch_size 8 \
 
     wait
 
