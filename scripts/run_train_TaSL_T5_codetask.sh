1589491a550c2d1735e772a947422377fbbe7353
Khanh Tran <ankhanhtran02@gmail.com>
2025-12-01 07:56:33 +0700

    Update script to run on codet5p-770m

--- scripts/run_train_TaSL_T5_codetask.sh
+++ scripts/run_train_TaSL_T5_codetask.sh
@@ -8,6 +8,7 @@ for ((ORDER=$begin_id; ORDER<4; ORDER++))
 do
 
     python finetune_continualDST_T5_codetask.py \
+        --model_path Salesforce/codet5p-770m \
         --num_epochs 5 \
         --train_size -1 \
         --val_size 100 \
@@ -20,5 +21,8 @@ do
 
     python skill_consolidation_T5_codetask.py \
         --service_begin_id=${ORDER} \
+        --checkpoint_name codet5p-770m-CodeTask-CL \
+        --ipt_file_name codet5p-770m-Importance_Score \
+        --model_name codet5p-770m \
 
 done
