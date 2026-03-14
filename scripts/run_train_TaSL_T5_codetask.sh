55cf9e466ba2b1e762ee3cb18e34532c8dd0f458
Khanh Tran <ankhanhtran02@gmail.com>
2026-03-14 10:29:54 +0700

    Edit script

--- scripts/run_train_TaSL_T5_codetask.sh
+++ scripts/run_train_TaSL_T5_codetask.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 begin_id=0
-
+export CUDA_VISIBLE_DEVICES=5
 
 for ((ORDER=$begin_id; ORDER<4; ORDER++))
 do
 
     python finetune_continualDST_T5_codetask.py \
         --model_path Salesforce/codet5p-770m \
-        --num_epochs 5 \
+        --num_epochs 3 \
         --train_size -1 \
         --val_size -1 \
         --task_list CONCODE CodeTrans CodeSearchNet BFP \
         --service_begin_id=${ORDER} \
-        --train_batch_size 32 \
-        --eval_batch_size 32 \
+        --train_batch_size 16 \
+        --eval_batch_size 16 \
 
     wait
 
